9b35f7efdd2a4334af5b40d9c47262eb404ab9f5
Gonzalo Monedero <gonza50.mh@gmail.com>
2021-12-03 18:16:43 +0100

    Añadida estructura

---
+++ MontecarloAlgoritmo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MontecarloAlgoritmo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
