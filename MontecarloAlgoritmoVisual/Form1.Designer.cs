3eda1fafbb0cfc09c53d074b65f4d5edd1371cce
Gonzalo Monedero <gonza50.mh@gmail.com>
2021-12-04 10:59:38 +0100

    Ya funciona el print, falta añadir popup para introducir número de bolas

--- MontecarloAlgoritmoVisual/Form1.Designer.cs
+++ MontecarloAlgoritmoVisual/Form1.Designer.cs
@@ -35,23 +35,26 @@ namespace MontecarloAlgoritmoVisual
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(588, 50);
+            this.label1.Location = new System.Drawing.Point(402, 53);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 16);
+            this.label1.Size = new System.Drawing.Size(41, 16);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Balls left: ";
+            this.label1.Text = "Out: ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(588, 83);
+            this.label2.Location = new System.Drawing.Point(402, 80);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(29, 16);
             this.label2.TabIndex = 2;
@@ -61,7 +64,7 @@ namespace MontecarloAlgoritmoVisual
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(588, 120);
+            this.label3.Location = new System.Drawing.Point(402, 105);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(29, 16);
             this.label3.TabIndex = 3;
@@ -69,7 +72,7 @@ namespace MontecarloAlgoritmoVisual
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(618, 294);
+            this.button1.Location = new System.Drawing.Point(432, 226);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(122, 39);
             this.button1.TabIndex = 4;
@@ -79,19 +82,47 @@ namespace MontecarloAlgoritmoVisual
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(618, 360);
+            this.button2.Location = new System.Drawing.Point(432, 284);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(122, 39);
             this.button2.TabIndex = 5;
-            this.button2.Text = "Add x10 balls";
+            this.button2.Text = "Throw balls";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(449, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(449, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(449, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 8;
             // 
             // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.ClientSize = new System.Drawing.Size(781, 437);
+            this.ClientSize = new System.Drawing.Size(564, 390);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
@@ -99,6 +130,7 @@ namespace MontecarloAlgoritmoVisual
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -111,6 +143,9 @@ namespace MontecarloAlgoritmoVisual
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- MontecarloAlgoritmoVisual/Form1.cs
+++ MontecarloAlgoritmoVisual/Form1.cs
@@ -7,23 +7,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Drawing;
+using System.Diagnostics;
+using System.Threading;
 
 namespace MontecarloAlgoritmoVisual
 {
     public partial class FormPrincipal : Form
     {
+        public static double ballsGlobal = 10000;
+
         public FormPrincipal()
         {
             InitializeComponent();
-            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             Graphics g = CreateGraphics();
-            g.DrawRectangle(new Pen(Color.Black, 3), 50, 20, 400, 400);
-            g.DrawEllipse(new Pen(Color.Red, 3), 50, 20, 400, 400);
+            g.DrawRectangle(new Pen(Color.Black, 3), 50, 50, 300, 300);
+            g.DrawEllipse(new Pen(Color.Red, 3), 50, 50, 300, 300);
+            button1.Enabled = false;
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double dotsin = 0; // dots in 
+            int dotsout = 0; // dots out
+            double pi = 0; // PI estimated
+            int x = 0; // Coordinate x
+            int y = 0; // Coordinate y
+
+            Random r = new Random();
+            Pen p1;
+            Graphics g2 = CreateGraphics();
+
+            for(int i = 0; i < ballsGlobal; i++)
+            {
+                // Generamos un punto aleatorio en (x,y)
+                x = r.Next(50, 351);
+                y = r.Next(50, 351);
+
+                // Center of circle is at (250,220)
+
+                int radius = (int)(Math.Sqrt(Math.Pow(x - 200, 2) + Math.Pow(y - 200, 2)));
+
+                if (radius <= 150)
+                { // Dot in
+                    p1 = new Pen(Color.Green);
+                    dotsin++;
+                }
+                else
+                {
+                    p1 = new Pen(Color.Orange);
+                    dotsout++;
+                }
+
+                g2.DrawRectangle(p1, x, y, 1, 1);
+            }
+
+            pi = (4* dotsin) / ballsGlobal;
+            Console.WriteLine(pi);
+
+            label4.Text = dotsout.ToString();
+            label5.Text = dotsin.ToString();
+            label6.Text = pi.ToString();
+
+        }
+
     }
 }
